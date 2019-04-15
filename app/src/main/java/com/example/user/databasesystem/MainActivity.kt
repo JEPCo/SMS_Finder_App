e067afedf9f1b0bd637205cc7845987db8ba5e4e
Osama AlHurani <alhurani93@gmail.com>
2019-04-15 19:12:24 +0300

    Permissions for >=SDK_23

--- app/src/main/java/com/example/user/databasesystem/MainActivity.kt
+++ app/src/main/java/com/example/user/databasesystem/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.user.databasesystem
 
 import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +16,16 @@ class MainActivity : AppCompatActivity() {
 
         if (Build.VERSION.SDK_INT>22)
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECEIVE_SMS, Manifest.permission.SEND_SMS, Manifest.permission.READ_CONTACTS), 95)
-        
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode==95)
+        {
+            if (grantResults[0]==PackageManager.PERMISSION_GRANTED && grantResults[1]==PackageManager.PERMISSION_GRANTED && grantResults[2]==PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this, "Thank you", Toast.LENGTH_LONG).show()
+            else
+                Toast.makeText(this, "Sorry you can't use this app", Toast.LENGTH_LONG).show()
+        }
     }
 }
