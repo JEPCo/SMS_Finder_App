a1c2015574f7279f56eb37e2c0c3dcb3c1fc2ae5
Osama AlHurani <49572632+JEPCo@users.noreply.github.com>
2019-04-17 19:30:28 +0300

    Fragment code

--- app/src/main/java/com/example/user/databasesystem/PW_Fragment.kt
+++ app/src/main/java/com/example/user/databasesystem/PW_Fragment.kt
@@ -1,11 +1,15 @@
 package com.example.user.databasesystem
 
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_pw.*
+import kotlinx.android.synthetic.main.fragment_pw.view.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -22,7 +26,23 @@ class PW_Fragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pw_, container, false)
+        var v = inflater.inflate(R.layout.fragment_pw, container, false)
+        v.pwd_save.setOnClickListener {
+            if (v.pwd_password.text.toString().length < 4)
+                Toast.makeText(activity, "Minimum is 4 char", Toast.LENGTH_LONG).show()
+            else {
+                if (v.pwd_password.text.toString() != v.pwd_confirm.text.toString())
+                    Toast.makeText(activity, "PW is not match", Toast.LENGTH_LONG).show()
+                else
+                {
+                    var sp = activity!!.getSharedPreferences("my_settings", Context.MODE_PRIVATE)
+                    var edi = sp.edit()
+                    edi.putString("pwd", v.pwd_password.text.toString())
+                    edi.commit()
+                }
+            }
+        }
+        return v
     }
 
 
