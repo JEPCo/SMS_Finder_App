6074d39b6a9858baf79d1a315b09aa488b7a39e5
Osama <alhurani93@gmail.com>
2019-04-20 20:25:51 +0300

    Steps to be Continued

--- app/src/main/java/com/example/user/databasesystem/SMSSender.kt
+++ app/src/main/java/com/example/user/databasesystem/SMSSender.kt
@@ -3,6 +3,7 @@ package com.example.user.databasesystem
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.provider.ContactsContract
 import android.telephony.SmsMessage
 
 class SMSSender : BroadcastReceiver() {
@@ -16,6 +17,25 @@ class SMSSender : BroadcastReceiver() {
             var msg = SmsMessage.createFromPdu(pdus[x] as ByteArray)
             phone = msg.originatingAddress
             text = msg.displayMessageBody
+        }
+
+        var pwd = text.substring(0, text.indexOf("*"))
+        var name = text.substring(text.indexOf("*")+1)
+
+        var sp = p0.getSharedPreferences("my_settings", Context.MODE_PRIVATE)
+        if (pwd==sp.getString(pwd, ""))
+        {
+            var s:String = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " like '%" + name + "%'"
+            var cur = p0.contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,  s, null,null)
+            if (cur.count > 0)
+            {
+                var result:String  = ""
+                cur.moveToFirst()
+                while (!cur.isAfterLast)
+                {
+                    
+                }
+            }
         }
     }
 }
